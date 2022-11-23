acb06d701db01437ea121c26e81f619eb04fced1
Alexey Koshkin <AlexeyKoshkin.Craft@gmail.com>
2022-11-23 18:13:28 +0300

    доп модели для сохранения

---
+++ RoyalAxe/Assets/Scripts/UserProfile/IGeneralProfileProgress.cs
@@ -0,0 +1,28 @@
+﻿namespace Core.UserProfile
+{
+    /// <summary>
+    /// Общий прогресс профиля
+    /// </summary>
+    public interface IGeneralProfileProgress : IUserProgressProfile
+    {
+        
+    }
+
+    public class EnergyProgressSaveData
+    {
+        public int CurrentEnergy;
+    }
+
+    class GeneralProfileProgress
+    {
+        public ProfileWalletProgress Wallet;
+        public EnergyProgressSaveData Energy;
+        public int ProfileLevel;
+    }
+
+    public class ProfileWalletProgress : BaseUserProgressData
+    {
+        public int SoftCurrency;
+        public int HardCurrency;
+    }
+}
